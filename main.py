3c4ffb343a53f2794e0cdc62109f97f8c5a6fd42
chengzyn <chengzyn@gmail.com>
2021-05-12 16:18:12 +0800

    Completed problem 1

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def main:
-    pass
+def main():
+    problem001()
+
+
+def problem001():
+    total = 0
+    for i in range(1000):
+        if i % 3 == 0 or i % 5 == 0:
+            total += i
+    print(total)
+
 
 if __name__ == '__main__':
     main()
